b125ffa33c19d259ef2022e1fce787d6c35e1df0
zhoucheng <zhoucheng133@outlook.com>
2026-02-14 19:13:14 +0800

    修复一个方向的问题

--- go.mod
+++ go.mod
@@ -10,9 +10,11 @@ require (
 require (
 	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
 	github.com/ncruces/zenity v0.10.14 // indirect
 	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
 	golang.org/x/image v0.20.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
--- utils/core.go
+++ utils/core.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"image"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,29 +10,23 @@ import (
 	_ "image/png"
 
 	"github.com/chai2010/webp"
-	"github.com/nfnt/resize"
+	"github.com/disintegration/imaging"
 )
 
 func Convert(path string, width, height int, output string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
+	src, err := imaging.Open(path, imaging.AutoOrientation(true))
 	if err != nil {
 		return err
 	}
 
-	newImg := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
+	dst := imaging.Resize(src, width, height, imaging.Lanczos)
 
 	outFile, err := os.Create(output)
 	if err != nil {
 		return err
 	}
 	defer outFile.Close()
-	err = webp.Encode(outFile, newImg, &webp.Options{Lossless: false, Quality: 80})
+	err = webp.Encode(outFile, dst, &webp.Options{Lossless: false, Quality: 80})
 	if err != nil {
 		return err
 	}
